184905e3830a192ce7e41a7455ff47b95a7998b9
SergeyKardashev <angeldeep@yandex.ru>
2024-06-17 18:34:09 +0700

    feat: add Webpack and ESLint

---
+++ src/dog/constants.ts
@@ -0,0 +1,4 @@
+export const myDog = {
+    name: 'Шарик',
+    isCute: true,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-const music: string = 'Punk';
-console.log(music);
+import { myDog } from './dog/constants';
+
+console.log('Привет, меня зовут', myDog.name);
+
+if (myDog.isCute) {
+    console.log('Я классный пёс!');
+}
