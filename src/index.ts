0a553e3ef76a77ccc940941bc1b8c2a179541530
SergeyKardashev <angeldeep@yandex.ru>
2024-06-18 20:02:18 +0700

    fix: add eslintignore, fix configs

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,9 @@ console.log('Привет, меня зовут', myDog.name);
 if (myDog.isCute) {
   console.log('Я классный пёс!');
 }
+
+function dogName(param:string):string {
+  return param;
+}
+
+console.log(dogName(myDog.name));
