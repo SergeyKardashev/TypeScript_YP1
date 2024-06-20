ef77ffc5e7155eab72330b6dbf4ce2ed73f4df15
SergeyKardashev <angeldeep@yandex.ru>
2024-06-20 21:37:49 +0700

    feat: function that returns sum, function that has optional parameter and returns void

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,24 @@ function dogName(param:string):string {
 }
 
 console.log(dogName(myDog.name));
+
+let legs: number;
+legs = 4; 
+legs = legs + 1; 
+
+console.log(legs);
+
+function sumNumberOfLimbs(legs: number, arms: number): string {
+  return `I have ${legs + arms} limbs`;
+}
+console.log('sumNumberOfLimbs and console say: ' + sumNumberOfLimbs(2, 2));
+
+function sayNumberOfLimbs(legs: number, arms?: number):void {
+  if (arms == undefined) {
+    console.log(`Say function says: I have ${legs} limbs`);
+  } else {
+    console.log(`Say function says: I have ${legs + arms} limbs`);
+  }
+}
+sayNumberOfLimbs(2, 2);
+sayNumberOfLimbs(2);
